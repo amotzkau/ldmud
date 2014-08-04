68b2d4c382c9746d4caa5178a6b231a12f130c6e
Alexander Motzkau <a.motzkau@web.de>
2014-08-04 20:16:32 +0200

    Keep variable_index_offset and current_variables synchronized.
    According to the comments current_variables is just a shortcut
    for current_object->variables + variable_index_offset. But as
    a matter of fact variable_index_offset is only correct at the
    beginning of an external call. After that in same cases only
    current_variables is updated (the control stack only saves and
    restores current_variables, not variable_index_offset).
    This patch now keeps them both synchronized.

--- src/interpret.c
+++ src/interpret.c
@@ -7345,6 +7345,7 @@ push_control_stack ( svalue_t   *sp
     csp->catch_call = MY_FALSE;
     csp->pc = pc;
     csp->function_index_offset = function_index_offset;
+    csp->variable_index_offset = variable_index_offset;
     csp->current_variables = current_variables;
     csp->break_sp = break_sp;
 #ifdef EVAL_COST_TRACE
@@ -7380,6 +7381,7 @@ pop_control_stack (void)
     inter_context = csp->context;
 #endif /* USE_NEW_INLINES */
     function_index_offset = csp->function_index_offset;
+    variable_index_offset = csp->variable_index_offset;
     current_variables     = csp->current_variables;
     break_sp = csp->break_sp;
     csp--;
@@ -9171,6 +9173,7 @@ again:
         }
 
         function_index_offset = csp->function_index_offset;
+        variable_index_offset = csp->variable_index_offset;
         current_variables     = csp->current_variables;
         break_sp = csp->break_sp;
 #ifdef USE_NEW_INLINES
@@ -14125,7 +14128,7 @@ again:
         flags = setup_new_frame1(
           func_index,
           function_index_offset + inheritp->function_index_offset,
-          inheritp->variable_index_offset
+          variable_index_offset + inheritp->variable_index_offset
         );
         funstart = current_prog->program + (flags & FUNSTART_MASK);
         csp->funstart = funstart;
@@ -14140,7 +14143,9 @@ again:
         /* Finish the setup */
         fp = inter_fp;
         pc = funstart;
-        current_variables += variable_index_offset;
+        current_variables = current_object->variables;
+        if (current_variables)
+            current_variables += variable_index_offset;
         current_strings = current_prog->strings;
         csp->extern_call = MY_FALSE;
         break;
@@ -17390,6 +17395,7 @@ retry_for_shadow:
             current_prog = cache[ix].progp;
             current_strings = current_prog->strings;
             function_index_offset = cache[ix].function_index_offset;
+            variable_index_offset = cache[ix].variable_index_offset;
 #ifdef DEBUG
             if (!ob->variables && cache[ix].variable_index_offset)
                 fatal("%s Fatal: apply (cached) for object %p '%s' "
@@ -17399,7 +17405,7 @@ retry_for_shadow:
 #endif
             current_variables = ob->variables;
             if (current_variables)
-                current_variables += cache[ix].variable_index_offset;
+                current_variables += variable_index_offset;
             inter_sp = setup_new_frame2(funstart, inter_sp, allowRefs, MY_FALSE);
                         
             // check argument types
