dd1027300ba03078bff53ef24c1a848f9d5d6141
Alexander Motzkau <a.motzkau@web.de>
2026-02-15 16:27:19 +0100

    Fix switching from charmode to linemode with buffered input
    If an interactive switches back from charmode to linemode, but has still
    unprocessed characters in the command buffer, the driver would end up in
    an endless loop for this interactive (not processing any commands, but
    having 100% CPU usage) until the interactive switches back to charmode.

---
+++ test/t-comm-stop-charmode.c
@@ -0,0 +1,67 @@
+// Check that if we abort a charmode mid-buffer, the driver
+// will process the rest normally.
+
+#include "/inc/base.inc"
+#include "/inc/client.inc"
+
+#include "/sys/input_to.h"
+
+void run_client()
+{
+   write("Hello!");
+   call_out(#'write, __ALARM_TIME__, "\n");
+}
+
+void run_server()
+{
+    configure_interactive(this_object(), IC_TELNET_ENABLED, 1);
+
+    input_to("client_char_input", INPUT_CHARMODE);
+}
+
+void client_char_input(string str)
+{
+    if (str != "H")
+    {
+        msg("FAILURE: Didn't receive a single char, got %Q instead.\n", str);
+        shutdown(1);
+        return;
+    }
+
+    input_to("client_line_input");
+}
+
+void client_line_input(string str)
+{
+    if (str != "ello!")
+    {
+        msg("FAILURE: Didn't receive correct rest of the input, got %Q instead.\n", str);
+        shutdown(1);
+        return;
+    }
+
+    msg("Success.\n");
+    shutdown(0);
+}
+
+void abort()
+{
+    msg("FAILURE: Didn't receive input.\n");
+    shutdown(1);
+}
+
+void run_test()
+{
+    msg("\nRunning test for stopping charmode #1:\n"
+          "--------------------------------------\n");
+
+    connect_self("run_server", "run_client");
+
+    call_out(#'abort, 2*__ALARM_TIME__);
+}
+
+string *epilog(int eflag)
+{
+    run_test();
+    return 0;
+}
